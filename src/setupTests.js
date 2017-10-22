b166c8119a4088f63ebd8efad3bef6583f28498f
aluis <andreluis9214@gmail.com>
2017-10-22 13:03:28 -0200

    style: Remove setupTests.js from lint

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,8 @@
-
-// import 'jest-styled-components';
 import 'raf/polyfill';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import { createSerializer } from 'enzyme-to-json';
 
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 
 configure({ adapter: new Adapter() });
