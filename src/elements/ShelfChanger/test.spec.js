0d61e11801ba7405af1291b8e81c695c6a7add93
aluis <andreluis9214@gmail.com>
2017-10-22 12:47:44 -0200

    test: ShelfChanger component test coverage

--- src/elements/ShelfChanger/test.spec.js
+++ src/elements/ShelfChanger/test.spec.js
@@ -3,10 +3,49 @@ import {
     shallow,
 } from 'enzyme';
 
+import { Select } from './styles';
+
 import ShelfChanger from './index';
 
 describe('ShelfChanger component', () => {
-    it('should render without crashing', () => {
-        shallow(<ShelfChanger />);
+    const defaultShelfChangerProps = {
+        defaultValue: '',
+        onChangeShelf: jest.fn(),
+    };
+
+    it('should render if it has onChangeShelf prop', () => {
+        expect(shallow(
+            <ShelfChanger
+                {...defaultShelfChangerProps}
+            />,
+        )).toMatchSnapshot();
+    });
+
+    it('should call onChangeShelf prop if select value changes', () => {
+        const shelfChangerProps = {
+            onChangeShelf: jest.fn(() => ({
+                then: jest.fn((success) => {
+                    success();
+
+                    return {
+                        catch: failure => failure(),
+                    };
+                }),
+            })),
+        };
+
+        const shelfChanger = shallow(
+            <ShelfChanger
+                {...shelfChangerProps}
+            />,
+        );
+
+        shelfChanger.find(Select).simulate('change', {
+            target: {
+                value: 'read',
+            },
+        });
+
+        expect(shelfChangerProps.onChangeShelf).toHaveBeenCalled();
     });
 });
