34fd6a917aed0c081dac54e80810075cf158902c
aluis <andreluis9214@gmail.com>
2017-10-22 12:48:14 -0200

    test: Bookshelf component test coverage

--- src/modules/Bookshelf/test.spec.js
+++ src/modules/Bookshelf/test.spec.js
@@ -3,10 +3,54 @@ import {
     shallow,
 } from 'enzyme';
 
+import {
+    BookshelfTitle,
+} from './styles';
+
 import Bookshelf from './index';
 
 describe('Bookshelf component', () => {
+    const defaultBookshelfProps = {
+        books: [],
+        onChangeShelf: jest.fn(),
+        title: '',
+    };
+
     it('should render without crashing', () => {
-        shallow(<Bookshelf />);
+        expect(shallow(
+            <Bookshelf {...defaultBookshelfProps} />,
+        )).toMatchSnapshot();
+    });
+
+    it('should books component if there is one or more books in array', () => {
+        const bookshelfProps = {
+            onChangeShelf: jest.fn(),
+            books: [
+                {
+                    id: '1',
+                },
+                {
+                    id: '2',
+                },
+            ],
+        };
+
+        expect(shallow(
+            <Bookshelf {...bookshelfProps} />,
+        ).find('Book').length).toBe(2);
+    });
+
+    it('should render shelf title if it has title prop', () => {
+        const bookshelfProps = {
+            title: 'shelf-title',
+            onChangeShelf: jest.fn(),
+        };
+
+        expect(
+            shallow(
+                <Bookshelf {...bookshelfProps} />,
+            ).find(BookshelfTitle).dive().find('h2')
+                .text(),
+        ).toEqual('shelf-title');
     });
 });
