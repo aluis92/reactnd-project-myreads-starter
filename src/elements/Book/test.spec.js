e664bc769412d8120c6704f501634477a1c7ac8d
aluis <andreluis9214@gmail.com>
2017-10-22 12:44:52 -0200

    test: Book component test coverage

--- src/elements/Book/test.spec.js
+++ src/elements/Book/test.spec.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import {
     shallow,
+    mount,
 } from 'enzyme';
+
+import { BookCondition } from './styles';
 
 import Book from './index';
 
 describe('Book component', () => {
-    const bookProps = {
-        imageLinks: '',
+    const defaultBookProps = {
+        id: '',
+        imageLinks: {},
         authors: [],
         title: '',
+        onChangeShelf: jest.fn(),
     };
 
     it('should render without crashing', () => {
-        shallow(
+        expect(shallow(
             <Book
-                {...bookProps}
+                {...defaultBookProps}
             />,
-        );
+        )).toMatchSnapshot();
+    });
+
+    it('should to show added category', () => {
+        const bookProps = {
+            ...defaultBookProps,
+            added: 'read',
+        };
+
+        const book = shallow(<Book {...bookProps} />);
+
+        expect(book.find(BookCondition).dive().text()).toEqual('read');
+    });
+
+    it('should call onChangeShelf prop when ShelfChanger trigger onChangeShelf function', () => {
+        const bookProps = {
+            ...defaultBookProps,
+            onChangeShelf: jest.fn(),
+        };
+
+        const book = mount(<Book {...bookProps} />);
+
+        book.find('ShelfChanger').instance().props.onChangeShelf();
+
+        expect(bookProps.onChangeShelf).toHaveBeenCalled();
     });
 });
