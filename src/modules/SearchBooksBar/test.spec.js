6901c08910e20535c36154f377e93db3f0e7c71b
aluis <andreluis9214@gmail.com>
2017-10-22 12:49:15 -0200

    test: SearchBooksBar component test coverage

--- src/modules/SearchBooksBar/test.spec.js
+++ src/modules/SearchBooksBar/test.spec.js
@@ -3,12 +3,34 @@ import {
     shallow,
 } from 'enzyme';
 
+import { SearchInput } from './styles';
+
 import SearchBooksBar from './index';
 
 describe('SearchBooksBar component', () => {
-    it('should render without crashing', () => {
-        shallow(
-            <SearchBooksBar />,
+    const defaultSearchBooksBar = {
+        onSearch: jest.fn(),
+    };
+
+    it('should render without crashing if it has onSearch prop', () => {
+        expect(shallow(
+            <SearchBooksBar {...defaultSearchBooksBar} />,
+        )).toMatchSnapshot();
+    });
+
+    it('should call onSearch prop if input value changes', () => {
+        const searchBooksBarProps = {
+            onSearch: jest.fn(),
+        };
+
+        const searchBooksBar = shallow(
+            <SearchBooksBar {...searchBooksBarProps} />,
         );
+
+        searchBooksBar
+            .find(SearchInput)
+            .simulate('change', { target: { value: 'test' } });
+
+        expect(searchBooksBarProps.onSearch).toHaveBeenCalled();
     });
 });
